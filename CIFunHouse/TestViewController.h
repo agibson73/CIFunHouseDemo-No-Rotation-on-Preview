cbe2a7da34f90a7a86a29562ecdc0df2999d9da8
Alex Gibson <oakmonttech@gmail.com>
2017-03-13 06:47:52 -0400

    Apple CI Demo
    Apple CI Demo with changes to keep preview from rotating with UI

---
+++ CIFunHouse/TestViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestViewController.h
+//  CIFunHouse
+//
+//  Created by Alex Gibson on 3/12/17.
+//  Copyright © 2017 Apple Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestViewController : UIViewController
+
+@end
---
+++ CIFunHouse/TestViewController.m
@@ -0,0 +1,41 @@
+//
+//  TestViewController.m
+//  CIFunHouse
+//
+//  Created by Alex Gibson on 3/12/17.
+//  Copyright © 2017 Apple Inc. All rights reserved.
+//
+
+#import "TestViewController.h"
+
+@interface TestViewController ()
+
+@end
+
+@implementation TestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+    
+-(BOOL)shouldAutorotate{
+    return NO;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
